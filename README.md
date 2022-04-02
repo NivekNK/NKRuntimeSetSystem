f89ba1af8ee46e51c7e86e01a8b73e6ab238f898
Your Name <you@example.com>
2022-04-02 17:05:50 -0300

    Modified README and force type T to be a MonoBehaviour in NKRuntimeSet

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # NK RuntimeSet System
+
+This RuntimeSet system allows you to define classes of type "Singleton", which you can find and use in other classes. It comes with an example of the Manager class type.
+
+## NKManager Class
+```csharp
+using UnityEngine;
+
+namespace NK.RuntimeSetSystem
+{
+    public abstract class NKManager : MonoBehaviour
+    {
+        public NKManagerRuntimeSet RuntimeSet;
+
+        protected virtual void OnEnable()
+        {
+            if (RuntimeSet == null)
+                RuntimeSet = NKRuntimeSet.GetRuntimeSet<NKManagerRuntimeSet>();
+            RuntimeSet?.Add(this);
+        }
+
+        protected virtual void OnDisable()
+        {
+            RuntimeSet?.Remove(this);
+        }
+    }
+}
+```
+What you can do now is inherit the class NKManager and use search for the created class in the "RuntimeSet".
+
+```csharp
+using NK.RuntimeSetSystem;
+
+public class ClassA : NKManager
+{
+    public string Name = "ClassA";
+}
+```
+
+```csharp
+using NK.RuntimeSetSystem;
+
+public class ClassB : MonoBehaviour
+{
+    private void Start()
+    {
+        var runtimeSet = NKRuntimeSet.GetRuntimeSet<NKManagerRuntimeSet>();
+
+        if (runtimeSet.GetTypeOfRuntimeSet(out ClassA classA))
+            Debug.Log(classA.Name);
+    }
+}
+```
--- Scripts/NKRuntimeSet.cs
+++ Scripts/NKRuntimeSet.cs
@@ -7,7 +7,7 @@ namespace NK.RuntimeSetSystem
     public abstract class NKRuntimeSet : ScriptableObject
     {
         /// <summary>
-        /// Return the first encountered RuntimeSet of the specified type in "Resources/RuntimeSets" folder or null if none is find.
+        /// Returns the first encountered RuntimeSet of the specified type in "Resources/RuntimeSets" folder or null if none is found.
         /// </summary>
         /// <typeparam name="TRuntimeSet"></typeparam>
         /// <returns></returns>
@@ -17,7 +17,7 @@ namespace NK.RuntimeSetSystem
         }
     }
 
-    public abstract class NKRuntimeSet<T> : NKRuntimeSet
+    public abstract class NKRuntimeSet<T> : NKRuntimeSet where T : MonoBehaviour
     {
         public SerializedDictionary<string, T> Sets = new SerializedDictionary<string, T>();
 
@@ -33,6 +33,12 @@ namespace NK.RuntimeSetSystem
             return Sets.Remove(t.GetType().Name);
         }
 
+        /// <summary>
+        /// Search for a MonoBehaviour of type "TType" in the RuntimeSet.
+        /// </summary>
+        /// <typeparam name="TType"></typeparam>
+        /// <param name="set"></param>
+        /// <returns></returns>
         public bool GetTypeOfRuntimeSet<TType>(out TType set) where TType : T
         {
             bool retVal = Sets.TryGetValue(typeof(TType).Name, out T value);
